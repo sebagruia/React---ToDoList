3c72916ad4513e0a8049e1288b00f3c627d4d93c
sebagruia <sebastiangruia@gmail.com>
2019-11-27 19:08:22 +0100

    Updates

---
+++ precache-manifest.6b5912350ed2bb9be3bf6c2510d0d316.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5afcc71f4f514ef3c71ec102212ad77",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b5bc19647b623719da71",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "933e434cedf854430c01",
+    "url": "/static/css/main.ae80fd4f.chunk.css"
+  },
+  {
+    "revision": "b5bc19647b623719da71",
+    "url": "/static/js/2.daad0b4d.chunk.js"
+  },
+  {
+    "revision": "933e434cedf854430c01",
+    "url": "/static/js/main.0ad722cd.chunk.js"
+  },
+  {
+    "revision": "2eb5f0810f3d957883a9",
+    "url": "/static/js/runtime-main.f99ad821.js"
+  }
+]);
